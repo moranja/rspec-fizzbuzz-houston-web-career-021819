bba5a6059956ef4644f3b0bc279235209cf6e0f7
Adam Moran <jeffadammoran@gmail.com>
2019-02-06 21:18:40 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,9 +4,9 @@
 def fizzbuzz(int)
   if (int % 15 == 0)
     return "FizzBuzz"
-  elsif (int % 5 == 0)
-    return "Buzz"
   elsif (int % 3 == 0)
     return "Fizz"
+  elsif (int % 5 == 0)
+    return "Buzz"
   end
 end
